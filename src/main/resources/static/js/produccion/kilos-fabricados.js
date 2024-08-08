ec68e176b694a0973724218a56759eb1674d6987
joan <jleonocampo@gmail.com>
2024-08-08 13:52:16 -0500

    Feat: creacion grafico para mostrar kilos fabricados en produccion

---
+++ src/main/resources/static/js/produccion/kilos-fabricados.js
@@ -0,0 +1,54 @@
+async function actualizarKilosTotales() {
+    try {
+        const datos = await obtenerDatosKilosFabricados();
+        if (datos && datos.length > 0) {
+            const kilosTotales = calcularKilosTotales(datos);
+            const elementoKilosTotales = document.getElementById('kilosTotales');
+            if (elementoKilosTotales) {
+                elementoKilosTotales.textContent = kilosTotales.toFixed(2);
+            } else {
+                console.error('Elemento con ID "kilosTotales" no encontrado');
+            }
+        } else {
+            console.warn('No se obtuvieron datos para calcular los kilos totales');
+        }
+    } catch (error) {
+        console.error('Error al actualizar kilos totales:', error);
+    }
+}
+
+function calcularKilosTotales(datos) {
+    return datos.reduce((total, item) => total + (item.peso * item.cantFabricada), 0);
+}
+
+async function obtenerDatosKilosFabricados() {
+    try {
+        const piezasCT = await obtenerPiezasCtProceso();
+        console.log('Piezas CT:', piezasCT);
+        return piezasCT;
+    } catch (error) {
+        console.error('Error al obtener datos de kilos fabricados:', error);
+        return [];
+    }
+}
+
+// Llamar a actualizarKilosTotales inmediatamente y luego cada 2 minutos
+actualizarKilosTotales();
+setInterval(actualizarKilosTotales, 2 * 60 * 1000);
+
+async function obtenerPiezasCtProceso(){
+	if(configProceso != null){
+		try{
+			const response = await fetch(`/centros-trabajo/${centroTSelected.id}/${configProceso.id}/piezas-operarios-ct-proceso`)
+			if(!response.ok){
+				console.error("Error en la solicitud:", response.statusText);
+				throw new Error("Error en la asignacion de pieza")
+			}
+			const data = await response.json()
+			return data
+			
+		}catch (error){
+			console.log(error)
+		}			
+	}
+}
