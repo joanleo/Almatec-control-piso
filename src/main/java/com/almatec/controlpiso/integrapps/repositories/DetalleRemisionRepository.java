a16c4f2d612f96cdb50ab2b238af4211f9401db0
joan <jleonocampo@gmail.com>
2024-08-08 14:19:05 -0500

    Update: acualizacion si cambios

--- src/main/java/com/almatec/controlpiso/integrapps/repositories/DetalleRemisionRepository.java
+++ src/main/java/com/almatec/controlpiso/integrapps/repositories/DetalleRemisionRepository.java
@@ -11,16 +11,16 @@ import com.almatec.controlpiso.integrapps.entities.DetalleRemision;
 
 public interface DetalleRemisionRepository extends JpaRepository<DetalleRemision, Long> {
 
-	@Query(value = "SELECT   detalle_remision.id_detalle_remision AS IdDetalleRemision, detalle_remision.cant AS Cant, detalle_remision.fecha_creacion AS FechaCreacion, "
-			+ "detalle_remision.id_remision AS IdRemision, detalle_remision.id_item_op AS IdItemOp, items_op.descripcion AS Descripcion, items_op.cant_req AS CantSol, "
+	@Query(value = "SELECT   remision_detalle.id_detalle_remision AS IdDetalleRemision, remision_detalle.cant AS Cant, remision_detalle.fecha_creacion AS FechaCreacion, "
+			+ "remision_detalle.id_remision AS IdRemision, remision_detalle.id_item_op AS IdItemOp, items_op.descripcion AS Descripcion, items_op.cant_req AS CantSol, "
 			+ "items_op.cant_cumplida AS CantCumplida, items_op.cant_despacha AS CantDespachada, items_op.peso_unitario AS Peso, items_op.marca AS Marca,"
 			+ "items_fabrica.item_ue AS UndEmpaque "
-			+ "FROM  detalle_remision "
+			+ "FROM  remision_detalle "
 			+ "INNER JOIN items_op "
-			+ "ON detalle_remision.id_item_op = items_op.item_id "
+			+ "ON remision_detalle.id_item_op = items_op.item_id "
 			+ "LEFT JOIN items_fabrica "
 			+ "ON items_op.Item_fab_Id = items_fabrica.Item_fab_Id "
-			+ "WHERE   (detalle_remision.id_remision = :idRemision)", nativeQuery = true)
+			+ "WHERE   (remision_detalle.id_remision = :idRemision)", nativeQuery = true)
 	List<DetalleRemisionInterface> buscarDetallesPorIdRemision(@Param("idRemision") Long idRemision);
 
 }
