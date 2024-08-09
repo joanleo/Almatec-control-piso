acc378a09183ee3be9bc78a3973c316b2088964d
joan <jleonocampo@gmail.com>
2024-08-08 21:23:53 -0500

    Update: archivo general javascript personalizacion de alert

--- src/main/resources/static/js/general.js
+++ src/main/resources/static/js/general.js
@@ -1,3 +1,19 @@
-document.getElementById('bt').addEventListener('click', function(event){
-	console.log(event.target)
-})
+function mostrarAlert(mensaje, tipo) {
+    const alertElement = document.createElement('div');
+    alertElement.className = `alert alert-${tipo} alert-dismissible fade show`;
+    alertElement.role = 'alert';
+    alertElement.style.zIndex = '10000';
+    alertElement.style.position = 'fixed';
+    alertElement.style.top = '7rem';
+    alertElement.style.left = '50%';
+    alertElement.style.transform = 'translateX(-50%)';
+    alertElement.style.width = '90rem'; 
+    alertElement.style.maxWidth = '90%';
+    alertElement.innerHTML = `
+        <strong>${mensaje}</strong>
+        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>`;
+
+    document.body.appendChild(alertElement);
+
+    setTimeout(() => alertElement.remove(), 5000);
+}
