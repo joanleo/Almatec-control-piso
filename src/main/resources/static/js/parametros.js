afa79743e98d7f115519ebd9f65e970955f122c4
joan <jleonocampo@gmail.com>
2024-09-04 15:14:57 -0500

    Fix: se mejora la presentacion de la pagina de parametros y se arregla error al cambio de dispositivos

--- src/main/resources/static/js/parametros.js
+++ src/main/resources/static/js/parametros.js
@@ -1,18 +1,20 @@
 document.addEventListener('DOMContentLoaded', async function () {
-				await obtenerParametros()
-				
-				document.getElementById('guardarCambios').addEventListener('click', async function () {
-		            const parametros = [];
-		            document.querySelectorAll('#parametros-container input').forEach(input => {
-		                parametros.push({
-		                    id: input.getAttribute('data-id'),
-		                    nombre: input.getAttribute('data-nombre'),
-		                    valor: input.value
-		                })
-		            })
-		            console.log('Guardando cambios...')
-		            await guardarCambios(parametros)
-			})
+	await obtenerParametros()
+	
+	document.getElementById('guardarCambios').addEventListener('click', async function () {
+        const parametros = [];
+        document.querySelectorAll('#parametros-container input').forEach(input => {
+            parametros.push({
+                id: input.getAttribute('data-id'),
+                nombre: input.getAttribute('data-nombre'),
+                valor: input.value
+            })
+        })
+        console.log('Guardando cambios...')
+        await guardarCambios(parametros)
+	})
+	
+	window.addEventListener('resize', ajustarLayout)
 })
 
 async function obtenerParametros() {
@@ -21,17 +23,28 @@ async function obtenerParametros() {
     const data = await response.json();
 
     const parametrosContainer = document.getElementById('parametros-container')
-    data.forEach(parametro => {
+	parametrosContainer.innerHTML = '';
+	
+    data.forEach((parametro, index) => {
         const div = document.createElement('div');
-        div.classList.add('input-group', 'my-3', 'mx-3')
-        div.style.width = '46%'
+        div.classList.add('input-group', 'my-3', 'mx-3', 'parametro-item')
+        //div.style.width = '46%'
+		div.classList.add(index % 2 === 0 ? 'item-left' : 'item-right');
         div.innerHTML = `
 		                <span class="input-group-text">${parametro.nombre}</span>
 						<input type="text" class="form-control" style="text-align: right;" value="${parametro.valor}" data-id="${parametro.id}" data-nombre="${parametro.nombre}"/>
 		            `;
         parametrosContainer.appendChild(div)
     })
+	
+	ajustarLayout()
+	
     spinner.setAttribute('hidden','')
+}
+
+function ajustarLayout() {
+    const parametrosContainer = document.getElementById('parametros-container');
+    parametrosContainer.classList.toggle('dos-columnas', window.innerWidth > 768);
 }
 
 async function guardarCambios(parametros){
