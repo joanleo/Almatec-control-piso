41d51365e325ea70fc80200e1f031e9978372814
joan <jleonocampo@gmail.com>
2024-09-12 10:11:14 -0500

    Fix: se corrige error que permitia accsar a los endpoints de manera manual digitando la url

--- src/main/java/com/almatec/controlpiso/config/SecurityConfig.java
+++ src/main/java/com/almatec/controlpiso/config/SecurityConfig.java
@@ -6,6 +6,7 @@ import java.util.Arrays;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.MediaType;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -66,6 +67,40 @@ public class SecurityConfig {
                 		.antMatchers("/centros-trabajo/generar-codigos-barra/**").permitAll()
                 		.antMatchers("/ingenieria/crear-op/**").permitAll()
                 		.antMatchers("/logout").permitAll()
+                		.antMatchers("/comercial").hasAuthority("ACCESS_PEDIDOS")
+                        .antMatchers("/comercial/pedidos/**").hasAuthority("ACCESS_ESTADO_PEDIDOS")
+                        .antMatchers("/comercial/crear-op/**").hasAuthority("ACCESS_ESTADO_PEDIDOS")
+                        .antMatchers("/ingenieria/status/proyectos").hasAuthority("ACCESS_ESTATUS_DE_PROYECTOS")
+                        .antMatchers("/ingenieria/op/**").hasAuthority("ACCESS_ESTATUS_DE_PROYECTOS")
+                        .antMatchers("/ingenieria/memos/nuevo").hasAuthority("ACCESS_CREAR_MEMO")
+                        .antMatchers(HttpMethod.POST, "/ingenieria/memos").hasAuthority("ACCESS_CREAR_MEMO")
+                        .antMatchers(HttpMethod.POST, "/ingenieria/memos/{idMemo}/aprobar-memo").hasAuthority("ROLE_APROBAR_MEMO")
+                        .antMatchers("/ingenieria/memos/aprobar").hasAuthority("ACCESS_APROBAR_MEMO")
+                        .antMatchers("/ingenieria/memos").hasAuthority("ACCESS_HISTORIAL_MEMOS")
+                        .antMatchers("/programacion/**").hasAuthority("ACCESS_PRIORIDAD")
+                        .antMatchers("/programacion/consulta-materia-prima").hasAuthority("ACCESS_CONSULTA_MATERIA_PRIMA_PROGRAMACION")
+                        .antMatchers("/programacion/resumen-fabricado").hasAuthority("ACCESS_RESUMEN_FABRICADO")
+                        .antMatchers("/produccion/**").hasAuthority("ACCESS_PLANTA")
+                        .antMatchers("/centros-trabajo/carga").hasAuthority("ACCESS_CAGA_X_CT")
+                        .antMatchers("/produccion/materia-prima/solicitud").hasAuthority("ACCESS_CREAR_SOLICITUD_MATERIA_PRIMA")
+                        .antMatchers("/produccion/operarios-registrados").hasAuthority("ACCESS_OPERARIOS_REGISTRADOS")
+                        .antMatchers("/produccion/impresion-etiquetas").hasAuthority("ACCESS_IMPRESION_DE_ETIQUETAS")
+                        .antMatchers("/informes/produccion/excel-general").hasAuthority("ACCESS_GENERAR_INFORME_GENERAL")
+                        .antMatchers("/informes/produccion/excel-mes").hasAuthority("ACCESS_GENERAR_INFORME_MENSUAL")
+                        .antMatchers("/almacen/solicitudes/**").hasAuthority("ACCESS_VER_SOLICITUDES_DE_MATERIA_PRIMA")
+                        .antMatchers("/almacen/detalle/**").hasAuthority("ACCESS_VER_SOLICITUDES_DE_MATERIA_PRIMA")
+                        .antMatchers("/almacen/remisiones/nueva").hasAuthority("ACCESS_CREAR_REMISION")
+                        .antMatchers("/almacen/remisiones/**").hasAuthority("ACCESS_VER_REMISIONES")
+                        .antMatchers("/almacen/consulta-materia-prima").hasAuthority("ACCESS_CONSULTA_MATERIA_PRIMA_ALMACEN")
+                        .antMatchers("/calidad/formularios").hasAuthority("ACCESS_LISTAR_REPORTES_CALIDAD")
+                        .antMatchers("/centros-trabajo").hasAuthority("ACCESS_CENTROS_DE_TRABAJO")
+                        .antMatchers("/operarios/listar").hasAuthority("ACCESS_OPERARIOS")
+                        .antMatchers("/operarios/editar/*").hasAuthority("ACCESS_OPERARIOS")
+                        .antMatchers(HttpMethod.POST, "/operarios/guardar").hasAuthority("ACCESS_OPERARIOS")
+                        .antMatchers(HttpMethod.DELETE, "/operarios/*").hasAuthority("ACCESS_OPERARIOS")
+                        .antMatchers("/roles/**").hasAuthority("ACCESS_PERFILES_O_ROLES")
+                        .antMatchers("/usuarios/**").hasAuthority("ACCESS_USUARIOS")
+                        .antMatchers("/parametros/listar").hasAuthority("ACCESS_PARAMETROS")
                         .antMatchers("/**").authenticated()
                         .anyRequest().authenticated())
                 .formLogin(login -> login
